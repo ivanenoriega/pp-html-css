fadebec27d62103cd3bdd0ee3cbf38889a818462
ivanenoriega <ivanenoriega@gmail.com>
2021-01-30 22:15:18 -0300

    eslint corrections added

--- pages/api/utils/auth0.js
+++ pages/api/utils/auth0.js
@@ -1,12 +1,13 @@
-import { initAuth0 } from "@auth0/nextjs-auth0";
+/* eslint-disable max-len */
+import { initAuth0 } from '@auth0/nextjs-auth0';
 
 export default initAuth0({
   domain: process.env.AUTH0_DOMAIN,
   clientId: process.env.AUTH0_CLIENT,
   clientSecret: process.env.AUTH0_SECRET,
-  scope: "openid profile",
-  redirectUri: "http://localhost:3000/api/callback",
-  postLogoutRedirectUri: "http://localhost:3000/",
+  scope: 'openid profile',
+  redirectUri: 'http://localhost:3000/api/callback',
+  postLogoutRedirectUri: 'http://localhost:3000/',
   session: {
     // The secret used to encrypt the cookie.
     cookieSecret: process.env.AUTH0_COOKIE,
@@ -15,7 +16,7 @@ export default initAuth0({
     // (Optional) The cookie domain this should run on. Leave it blank to restrict it to your domain.
     // cookieDomain: "your-domain.com",
     // (Optional) SameSite configuration for the session cookie. Defaults to 'lax', but can be changed to 'strict' or 'none'. Set it to false if you want to disable the SameSite setting.
-    cookieSameSite: "lax",
+    cookieSameSite: 'lax',
     // (Optional) Store the id_token in the session. Defaults to false.
     storeIdToken: false,
     // (Optional) Store the access_token in the session. Defaults to false.
